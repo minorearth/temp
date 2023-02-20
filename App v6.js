713e74cc78d38361c998a19a9b521221c9a1a787
RomanInformatika <officepm@yandex.ru>
2023-02-20 19:14:09 +0300

    fetch added

---
+++ App v6.js
@@ -0,0 +1,136 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { useState, useEffect, useRef } from 'react';
+// import { TouchableOpacity } from 'react-native-web';
+
+export default function App() {
+
+  const [FN, setFN] = useState('')
+  const [LN, setLN] = useState('')
+  const [PSW, setPSW] = useState('')
+  const [EM, setEM] = useState('')
+  const [PSWC, setPSWC] = useState('')
+  const [errorM, seterrorM] = useState('Please provide signup data')
+
+  const CheckProps = () => {
+
+    if (FN == '') {
+
+      seterrorM('Please Enter first name')
+
+    } else if (PSWC != PSW) {
+      seterrorM('Wrong passcode')
+
+
+    }
+    else if (!EM.includes('@') || !EM.includes('.')) {
+      seterrorM('Incorrect mail')
+
+
+    }
+    else {
+      seterrorM('Please provide signup data')
+
+
+    }
+
+
+
+
+  }
+
+  return (
+    <View style={styles.main}>
+      <Text>First name</Text>
+      <TextInput style={styles.edit}
+        placeholder="First"
+        secureTextEntry='true'
+        value={FN}
+        onChangeText={(value) => setFN(value)}
+      ></TextInput>
+      <Text>Last name</Text>
+      <TextInput style={styles.edit}
+        value={LN}
+        onChangeText={(value) => setLN(value)}
+      ></TextInput>
+      <Text>e-mail adress</Text>
+      <TextInput style={styles.edit}
+        value={EM}
+        onChangeText={(value) => setEM(value)}
+      ></TextInput>
+      <Text>Password</Text>
+      <TextInput style={styles.edit}
+        value={PSW}
+        onChangeText={(value) => setPSW(value)}
+      ></TextInput>
+      <Text>Confirm passport</Text>
+      <TextInput style={styles.edit}
+        value={PSWC}
+        onChangeText={(value) => setPSWC(value)}
+      ></TextInput>
+
+      <TouchableOpacity
+        onPress={() => CheckProps()}
+        style={styles.btn}
+
+
+      >
+        <Text >Sign Up</Text>
+      </TouchableOpacity>
+      <Text>{errorM}</Text>
+
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  btn: {
+    height: 50,
+    backgroundColor: "#AAAAAA",
+    width: "50%",
+    margin: 20,
+    borderRadius: 15,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+
+
+
+
+  },
+  edit: {
+
+
+    backgroundColor: "#FFFFFF",
+    width: "70%",
+    height: 50,
+    borderRadius: 10,
+  },
+
+  main: {
+    flex: 1,
+    backgroundColor: "#FF00AA",
+    flexDirection: "column",
+    padding: 10,
+    alignItems: "center",
+    paddingTop: 50,
+  },
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- App.js
+++ App.js
@@ -1,58 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
-import { useState,useEffect,useRef } from 'react';
+import { Text, View, } from 'react-native';
+import { useState, useEffect } from 'react';
+
+
 
 export default function App() {
 
-  const [EditText, setEditText]=useState(1000)
+  const ff = () => {
+
+    const zu = fetch('http://mskko2021.mad.hakta.pro/api/user/login',
+      {
+        method: "POST",
+        headers: { 'Content-Type': 'text/plain' },
+        body: JSON.stringify({
+          "email": "junior@wsr.ru",
+          "password": "junior"
+        })
+
+      }).then(res => res.json()).then(res => console.log(res))
+    return zu
+
+  }
+
+
+  useEffect(() => {
+    const zu=ff()
+    console.log('here we go',zu)
 
 
 
-  
-
+  }, [])
 
   return (
-    <View style={styles.main}>
-      <TextInput style={styles.edit}
-        value={EditText}
-        onChangeText = {(value)=>setEditText(value)}
 
-      >
-      </TextInput>
-      <Text style={{width: Number(EditText), backgroundColor: '#FFFFFF'}}> Hello</Text>
-      <Text>{EditText}</Text>
-      <Text>{EditText}</Text>
-      <Text>{EditText}</Text>
-      <Text>{EditText}</Text>
-      <Text>{EditText+'asdasd'}</Text>
+    <View>
+      <Text>
+            blah
 
-    
-      <Text>Hello</Text>
+      </Text>
+
+
 
     </View>
 
-  );
+
+  )
+
+
 }
-
-const styles = StyleSheet.create({
-edit:{
-
-
-  backgroundColor: "#FFFFFF",
-  width: "70%",
-  height: 50,
-  borderRadius:10,
-},
-
-  main: {
-    flex: 1,
-    backgroundColor: "#FF00AA",
-    flexDirection: "column",
-    padding: 10,
-    alignItems: "flex-start",
-    paddingTop:50,
-  },
-
-
-
-});
