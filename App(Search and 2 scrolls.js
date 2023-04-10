75e131d73d3638d11f444a0e2522d8f81048697e
RomanInformatika <officepm@yandex.ru>
2023-04-10 12:46:11 +0300

    grazyForm

--- App(Search and 2 scrolls.js
+++ App(Search and 2 scrolls.js
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react';
 import { FlatList, Text, View, TouchableOpacity, StyleSheet, Image, TextInput, SafeAreaView, ScrollView } from 'react-native';
-import { CART, PATIENTS } from './data';
-
 
 
 const getNews = (setnewsdata) => {
@@ -37,6 +35,8 @@ export const NewsImage = ({ image }) => {
 
 
 export const ProductImage = ({ image }) => {
+
+  console.log(image)
   return (
     <Image
       source={{
@@ -56,19 +56,31 @@ export const News = ({ newsd }) => {
   return (
     <FlatList
       data={newsd}
+     
       renderItem={({ item }) => <NewsImage image={item.image} />}
       keyExtractor={(item, index) => index.toString()}
       horizontal
-
+      
     />
   )
 
 }
 
-export const Products = ({ newsd, setshowNews }) => {
+
+
+export const Products = ({ newsd,setshowNews }) => {
+
+
+
   const handleScroll = (event) => {
-    event.nativeEvent.contentOffset.y > 200 ? setshowNews(false) : setshowNews(true)
+    console.log(event.nativeEvent.contentOffset.y)
+    event.nativeEvent.contentOffset.y>200?setshowNews(false):setshowNews(true)
+
+    // const positionX = event.nativeEvent.contentOffset.x;
+    // const positionY = event.nativeEvent.contentOffset.y;
   };
+
+
   if (newsd.length == 0) {
     return <Text>...Loading</Text>
   }
@@ -77,8 +89,9 @@ export const Products = ({ newsd, setshowNews }) => {
       data={newsd}
       renderItem={({ item }) => <ProductImage image={item.image} />}
       keyExtractor={(item, index) => index.toString()}
-      style={{ width: "100%" }}
-      onScroll={(event) => handleScroll(event)}
+      style={{width:"100%"}}
+      onScroll={(event)=>handleScroll(event)}
+      
     />
   )
 
@@ -98,99 +111,43 @@ const SearchBar = () => {
 
   </View>
   )
-}
-
-
-
-const CheckedItem = ({ item, orders, cart, person, setData }) => {
-  const checkFull = (id, orders, cart) => {
-    const qty = cart.filter(item => item.id == id)[0]['qty']
-    const ordersQty = [].concat(...orders.map(itm => itm.order)).filter(itm => itm.assign == 'checked' && itm.id == item.id).length
-    ordersQty == qty ? orders
-      .filter(nm => nm.name != person)
-      .forEach(person => person.order
-        .filter(itm => itm.id == item.id && itm.assign == 'notChecked')
-        .forEach(item => item.assign = 'disabled')) :
-      orders.forEach(person => person.order.filter(itm => itm.id == item.id && itm.assign == 'disabled').forEach(item => item.assign = 'notChecked'))
-    setData(data => [...data])
-  }
-
-  const assign = (item) => {
-    if (item.assign == 'checked') {
-      item.assign = 'notChecked'
-    } else if (item.assign == 'notChecked') {
-      item.assign = 'checked'
-    }
-    checkFull(item.id, orders, cart)
-
-  }
-
-  return (
-    <TouchableOpacity style={{ backgroundColor: "#FFAA00" }}
-      onPress={() => assign(item)}>
-      <Text> {`${item.name}  ${item.assign}`} </Text>
-    </TouchableOpacity>
-  )
-}
-
-const OrderSetup = ({ order, orders, cart, setData }) => {
-  const [hide, setHide] = useState(false)
-
-  return (
-    <View style={{ width: '100%', backgroundColor: "#FF0000", flexDirection: "column" }}>
-      <TouchableOpacity
-        style={{ height: 50, backgroundColor: "#FF00AA" }}
-        onPress={() => setHide(!hide)}>
-        <Text>{order["name"]}</Text>
-      </TouchableOpacity>
-      {hide && <FlatList
-        data={order.order}
-        renderItem={item => <CheckedItem
-          item={item.item}
-          orders={orders}
-          cart={cart}
-          person={order.name}
-          setData={setData}
-        />}
-
-      ></FlatList>}
-    </View>
-
-
-  )
-
-
 
 }
+
+
 export default function App() {
   const [newsdata, setnewsdata] = useState([]);
-  const [showNews, setshowNews] = useState(true)
-  const [data, setData] = useState()
-  const formOrder = (CART, PATIENTS) => {
-    return PATIENTS.map(item => ({ name: item, order: CART.map(item => ({ ...item, assign: 'notChecked' })) }))
-  }
+  const [showNews,setshowNews]=useState(true)
   useEffect(() => {
     getNews(setnewsdata)
-    setData(formOrder(CART, PATIENTS))
   }, [])
+
+  const ssspl=(ss)=>{
+
+    return ss.replaceAll('.','.\n').replaceAll('!','!\n').replaceAll('?','?\n')
+
+  }
+
   return (
+    // <View >
     <SafeAreaView style={styles.container}>
-      {false && <View style={styles.topbar}>
+      <View style={styles.topbar}>
         <SearchBar />
-        {showNews && <News newsd={newsdata} />}
-      </View>}
-      {false && <Products newsd={newsdata} setshowNews={setshowNews} />}
-      <Text></Text>
-      <FlatList
-        data={data}
-        renderItem={(item) => <OrderSetup
-          order={item.item}
-          orders={data}
-          cart={CART}
-          setData={setData}
-        />}
-      ></FlatList>
+        {showNews&& <News newsd={newsdata} />}
+      </View>
+      <Text>{ssspl('Hello World!This is. my? string')}</Text>
+      {/* <ScrollView>
+        <Text style={{height:200}}>asdasd</Text>
+        <Text style={{height:200}}>asdasd</Text>
+        <Text style={{height:200}}>asdasd</Text>
+        <Text style={{height:200}}>asdasd</Text>
+        <Text style={{height:200}}>asdasd</Text>
+        <Text style={{height:200}}>asdasd</Text>
+      </ScrollView> */}
+      <Products newsd={newsdata} setshowNews={setshowNews} />
+
     </SafeAreaView>
+    // </View>
   )
 }
 
@@ -202,8 +159,11 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "flex-start",
     backgroundColor: "#FF00FF"
+
+
   },
   NewsImage: {
+
     width: 200,
     aspectRatio: 1.5,
     backgroundColor: "#FFFFFF",
@@ -218,6 +178,8 @@ const styles = StyleSheet.create({
     margin: 10,
     borderRadius: 10
   },
+
+
   input: {
     width: 300,
     height: 40,
@@ -235,6 +197,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     borderRadius: 5,
     margin: 5,
+    // borderRadius: 10,
   },
 
   container: {
